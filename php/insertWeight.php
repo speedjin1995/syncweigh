94259930e23206edd3be41fde8de0edb9ff9343c
laiwoontzer <darrenlai95@gmail.com>
2022-02-19 16:49:46 +0800

    test for db

--- php/insertWeight.php
+++ php/insertWeight.php
@@ -11,19 +11,54 @@ session_start();
 	$role = $user->getId();
 }*/
 
-if(isset($_POST['status'], $_POST['invoiceNo'], $_POST['remark'])){
+if(isset($_POST['status'], $_POST['lotNo'], $_POST['invoiceNo'], $_POST['vehicleNo'],$_POST['customerNo'],$_POST['deliveryNo'],$_POST['unitWeight']
+,$_POST['batchNo'],$_POST['purchaseNo'],$_POST['currentWeight'],$_POST['product'],$_POST['moq'],$_POST['tareWeight'],$_POST['package'],$_POST['unitPrice'],$_POST['actualWeight']
+,$_POST['remark'],$_POST['totalPrice'],$_POST['totalWeight'])){
     // echo $_POST['status'].' ';
     // echo $_POST['invoiceNo'].' ';
     // echo $_POST['remark'].' ';
+	$serialNo = "S000001";
+	$status = filter_input(INPUT_POST, 'status', FILTER_SANITIZE_STRING);
+	$lotNo = filter_input(INPUT_POST, 'lotNo', FILTER_SANITIZE_STRING);
+	$invoiceNo = filter_input(INPUT_POST, 'invoiceNo', FILTER_SANITIZE_STRING);
+	$vehicleNo = filter_input(INPUT_POST, 'vehicleNo', FILTER_SANITIZE_STRING);
+	$customerNo = filter_input(INPUT_POST, 'customerNo', FILTER_SANITIZE_STRING);
+	$deliveryNo = filter_input(INPUT_POST, 'deliveryNo', FILTER_SANITIZE_STRING);
+	$unitWeight = filter_input(INPUT_POST, 'unitWeight', FILTER_SANITIZE_STRING);
+	$batchNo = filter_input(INPUT_POST, 'batchNo', FILTER_SANITIZE_STRING);
+	$purchaseNo = filter_input(INPUT_POST, 'purchaseNo', FILTER_SANITIZE_STRING);
+	$currentWeight = filter_input(INPUT_POST, 'currentWeight', FILTER_SANITIZE_STRING);
+	$product = filter_input(INPUT_POST, 'product', FILTER_SANITIZE_STRING);
+	$moq = filter_input(INPUT_POST, 'moq', FILTER_SANITIZE_STRING);
+	$tareWeight = filter_input(INPUT_POST, 'tareWeight', FILTER_SANITIZE_STRING);
+	$package = filter_input(INPUT_POST, 'package', FILTER_SANITIZE_STRING);
+	$unitPrice = filter_input(INPUT_POST, 'unitPrice', FILTER_SANITIZE_STRING);
+	$actualWeight = filter_input(INPUT_POST, 'actualWeight', FILTER_SANITIZE_STRING);
+	$remark = filter_input(INPUT_POST, 'remark', FILTER_SANITIZE_STRING);
+	$totalPrice = filter_input(INPUT_POST, 'totalPrice', FILTER_SANITIZE_STRING);
+	$totalWeight = filter_input(INPUT_POST, 'totalWeight', FILTER_SANITIZE_STRING);
+	$date = new DateTime();
+	$dateT = $date->format('Y-m-d');
 
-    echo json_encode(
-        array(
-            "status"=> $_POST['status'], 
-            "invoiceNo"=> $_POST['invoiceNo'],
-            "remark"=> $_POST['remark']
-
-        )
-    );
+	if ($insert_stmt = $db->prepare("INSERT INTO weight (serialNo, vehicleNo, lotNo, batchNo, invoiceNo, deliveryNo, purchaseNo, customer, product, package
+	, currentWeight, tareWeight, totalWeight, actualWeight, unitWeight, date, time, moq, unitPrice, totalPrice, remark, status) 
+	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")){
+		$insert_stmt->bind_param('ssssssssssssssssssssss', $serialNo, $vehicleNo, $lotNo, $batchNo, $invoiceNo, $deliveryNo, $purchaseNo, $customerNo, $product
+		, $package, $currentWeight, $tareWeight, $totalWeight, $actualWeight, $unitWeight, $dateT, null , $moq, $unitPrice, $totalPrice, $remark, $status);
+		
+		// Execute the prepared query.
+		if (! $insert_stmt->execute()){
+			// echo '<script type="text/javascript">alert("'.$insert_stmt->error.'");';
+			// echo 'location.href = "../register.html";</script>';
+		} else{
+			echo '<script type="text/javascript">alert("You registered successfully");';
+			// echo 'location.href = "../login.html";</script>';
+		}
+	} else{
+		// echo '<script type="text/javascript">alert("Something wrong with the server");';
+		echo '<script type="text/javascript">alert("'.$insert_stmt->error.'");';
+		// echo 'location.href = "../register.html";</script>';
+	}		
 
     // $email = filter_input(INPUT_POST, 'userEmail', FILTER_SANITIZE_EMAIL);
 	// $email = filter_var($email, FILTER_VALIDATE_EMAIL);
@@ -70,5 +105,5 @@ if(isset($_POST['status'], $_POST['invoiceNo'], $_POST['remark'])){
 	// }
 } else{
 	echo '<script type="text/javascript">alert("Please fill in all the fields");';
-	echo 'location.href = "../register.html";</script>';       
+	// echo 'location.href = "../register.html";</script>';       
 }
