3103c2080589887d26f730755ed29dad215fd799
laiwoontzer <darrenlai95@gmail.com>
2022-03-18 01:22:54 +0800

    done export excel

---
+++ php/export.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once 'db_connect.php';
+// // Load the database configuration file 
+ 
+// Filter the excel data 
+function filterData(&$str){ 
+    $str = preg_replace("/\t/", "\\t", $str); 
+    $str = preg_replace("/\r?\n/", "\\n", $str); 
+    if(strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"'; 
+} 
+ 
+// Excel file name for download 
+if($_GET["file"] == 'weight'){
+    $fileName = "Weight-data_" . date('Y-m-d') . ".xls";
+}else{
+    $fileName = "Count-data_" . date('Y-m-d') . ".xls";
+} 
+ 
+// Column names 
+if($_GET["file"] == 'weight'){
+    $fields = array('SERIAL NO', 'PRODUCT NO', 'UNIT', 'UNIT WEIGHT', 'TARE', 'TOTAL WEIGHT', 'ACTUAL WEIGHT', 'MOQ', 'UNIT PRICE(RM)', 'TOTAL PRICE(RM)',
+                'VEHICLE NO', 'LOT NO', 'BATCH NO', 'INVOICE NO', 'DELIVERY NO', 'PURCHASE NO', 'CUSTOMER', 'PACKAGE', 'DATE', 'REMARK'); 
+}else{
+    $fields = array('SERIAL NO', 'PRODUCT NO', 'UNIT', 'UNIT WEIGHT', 'TARE', 'CURRENT WEIGHT', 'ACTUAL WEIGHT', 'TOTAL PCS','MOQ', 'UNIT PRICE(RM)', 'TOTAL PRICE(RM)',
+    'VEHICLE NO', 'LOT NO', 'BATCH NO', 'INVOICE NO', 'DELIVERY NO', 'PURCHASE NO', 'CUSTOMER', 'PACKAGE', 'DATE', 'REMARK');    
+}
+ 
+// Display column names as first row 
+$excelData = implode("\t", array_values($fields)) . "\n"; 
+ 
+// Fetch records from database
+if($_GET["file"] == 'weight'){
+    $query = $db->query("SELECT * FROM weight WHERE deleted = '0' ORDER BY serialNo ASC");
+}else{
+    $query = $db->query("SELECT * FROM count WHERE deleted = '0' ORDER BY serialNo ASC");
+}
+
+if($query->num_rows > 0){ 
+    // Output each row of the data 
+    while($row = $query->fetch_assoc()){ 
+        // $status = ($row['status'] == 1)?'Active':'Inactive';
+        if($_GET["file"] == 'weight'){
+            $lineData = array($row['serialNo'], $row['productName'], $row['unit'], $row['unitWeight'], $row['tare'], $row['totalWeight'], $row['actualWeight'],
+            $row['moq'], $row['unitPrice'], $row['totalPrice'], $row['vehicleNo'], $row['lotNo'], $row['batchNo'], $row['invoiceNo']
+            , $row['deliveryNo'], $row['purchaseNo'], $row['customer'], $row['package'], $row['dateTime'], $row['remark']);
+        }else{
+            $lineData = array($row['serialNo'], $row['productName'], $row['unit'], $row['unitWeight'], $row['tare'], $row['currentWeight'], $row['actualWeight'],
+            $row['totalPCS'], $row['moq'], $row['unitPrice'], $row['totalPrice'], $row['vehicleNo'], $row['lotNo'], $row['batchNo'], $row['invoiceNo']
+            , $row['deliveryNo'], $row['purchaseNo'], $row['customer'], $row['package'], $row['dateTime'], $row['remark']);
+        }
+
+        array_walk($lineData, 'filterData'); 
+        $excelData .= implode("\t", array_values($lineData)) . "\n"; 
+    } 
+}else{ 
+    $excelData .= 'No records found...'. "\n"; 
+} 
+ 
+// Headers for download 
+header("Content-Type: application/vnd.ms-excel"); 
+header("Content-Disposition: attachment; filename=\"$fileName\""); 
+ 
+// Render excel data 
+echo $excelData; 
+ 
+exit;
+?>
