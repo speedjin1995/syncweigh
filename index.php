ded4c26159727c29ae1902588e468dbc3e1aa970
speedjin1995 <speedjin1995@gmail.com>
2022-05-13 20:00:28 +0800

    Changed UI
    Changed UI

--- index.php
+++ index.php
@@ -28,8 +28,9 @@ else{
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>Synctronix | Dashboard </title>
+  <title>SyncWeight </title>
 
+  <link rel="icon" href="assets/logoSmall.png" type="image">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
   <!-- IonIcons -->
@@ -115,10 +116,9 @@ to get the desired effect
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link">
-      <!--img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8"!-->
-      <span class="brand-text font-weight-light">Synctronix</span>
+    <a href="#" class="brand-link logo-switch">
+      <img src="assets/logoSmall.png" alt="Sneakercube Logo" class="brand-image-xl logo-xs">
+      <img src="assets/logo.png" alt="Sneakercube Logo" class="brand-image-xl logo-xl">
     </a>
 
     <!-- Sidebar -->
